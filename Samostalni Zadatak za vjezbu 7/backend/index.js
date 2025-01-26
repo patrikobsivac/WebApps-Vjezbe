66cdae7f27ed6b3b7246c466de6f77c1496cd6c5
Patrik Tovernić Obšivač <patriktovernic@gmail.com>
2025-01-26 01:21:50 +0100

    Update package.json

--- Samostalni Zadatak za vjezbu 7/backend/index.js
+++ Samostalni Zadatak za vjezbu 7/backend/index.js
@@ -1,12 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import tasksRouter from './routes/tasks.js';
+import authRouter from './routes/auth.js';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import {
+  hashPassword,
+  checkPassword,
+  generateJWT,
+  verifyJWT,
+} from './routes/auth.js';
+dotenv.config();
 
 const PORT = 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/auth', authRouter);
 app.use('/tasks', tasksRouter);
 app.get('/', (req, res) => {
   res.send('TaskManagerBackend');
